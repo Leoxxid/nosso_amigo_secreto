afb7979e4342f3f18586f667245595c37cd246d0
leoxxid <leoxxid@hotmail.com>
2018-07-05 00:21:22 -0300

    add controller home

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 require 'sidekiq/web'
 
 Rails.application.routes.draw do
+  get 'pages/home'
+
   devise_for :users, :controllers => { registrations: 'registrations' }
   mount Sidekiq::Web => '/sidekiq'
 
