df300fab655868f6d02d51afd7246d918bfdc4c4
leoxxid <leoxxid@hotmail.com>
2018-07-08 18:27:35 -0300

    rescue 404

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  before_action :authenticate_user!
+
+  rescue_from ActiveRecord::RecordNotFound, :with => :render_404
+
+  def render_404
+    redirect_to main_app.root_url
+  end
 end
