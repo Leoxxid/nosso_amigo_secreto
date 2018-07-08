8f626859585ffc241b57a37d3148af1def2d4564
leoxxid <leoxxid@hotmail.com>
2018-07-08 10:57:20 -0300

    set raffle job

---
+++ app/jobs/campaign_raffle_job.rb
@@ -0,0 +1,16 @@
+class CampaignRaffleJob < ApplicationJob
+  queue_as :emails
+
+  def perform(campaign)
+    results = RaffleService.new(campaign).call
+
+    campaign.members.each {|m| m.set_pixel}
+    results.each do |r|
+      CampaignMailer.raffle(campaign, r.first, r.last).deliver_now
+    end
+    campaign.update(status: :finished)
+
+    #if results == false
+      # Send mail to owner of campaign (desafio)
+    #end
+  end
