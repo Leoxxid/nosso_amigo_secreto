2a1add662bca0ab322f40c9b570cbf1fc46355d3
leoxxid <leoxxid@hotmail.com>
2018-07-16 02:04:47 -0300

    set digitalocean host to email

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -84,7 +84,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = { :host => 'http://localhost:3000' }
+  config.action_mailer.default_url_options = { :host => '45.55.48.199' }
   config.action_mailer.delivery_method = :smtp
 
   config.action_mailer.raise_delivery_errors = true
